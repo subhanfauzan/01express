9240fdf258aceafb7d581f63687c9e2fbbf5b0ae
subhanfauzan <ariel1181987@gmail.com>
2023-09-19 16:17:35 +0700

    Materi 5

--- routes/mahasiswa.js
+++ routes/mahasiswa.js
@@ -50,4 +50,59 @@ router.post('/store',[
         }
     })
 })
+
+router.get('/(:id)', function(req,res) {
+    let id = req.params.id;
+    connection.query(`select * from mahasiswa where id_m = ${id}`, function (err, rows) {
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'server eror',
+            })
+        }
+        if(rows.length <=0){
+            return res.status(404).json({
+                staus: false,
+                message: 'not Found',
+            })
+        }
+        else{
+            return res.status(200).json({
+                status: true,
+                message: 'Data Mahasiswa',
+                data: rows[0]
+            })
+        }
+    })
+})
+
+router.patch('/update/:id',[
+    body('nama').notEmpty(),
+    body('nrp').notEmpty()
+], (req, res) => {
+    const error = validationResult(req);
+    if(!error.isEmpty()){
+        return res.status(422).json({
+            error: error.array()
+        });
+    }
+    let id = req.params.id;
+    let Data = {
+        nama: req.body.nama,
+        nrp: req.body.nrp
+    }
+    connection.query(`update mahasiswa set ? where id_m = ${id}`, Data, function (err, rows){
+        if(err){
+            return res.status(500).json({
+                status: false,
+                message: 'Server failed', 
+            })
+        }else{
+            return res.status(200).json({
+                status: true,
+                message: 'Data Berhasil Diubah',
+            })
+        }
+    })
+})
 module.exports = router;
